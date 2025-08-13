35cc44e9a35b5b6ae8a4aba576364b9315b102b7
Guilherme C. Rikaczewski <guilhermecrikaczewski@gmail.com>
2025-08-13 17:39:52 -0300

    configura o index para rodar o servidor (FUNCIONANDO 100% FAZ O L)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3069
-const router = require("./routes")
+const app = require('./app')
+require('dotenv').config()
 
-app.use(router)
 
+const port = process.env.SERVER_PORT
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
