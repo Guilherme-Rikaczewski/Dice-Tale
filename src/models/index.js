243ed7a7365e61d8ffbafbe5f8fa38c0930b1ca4
Guilherme C. Rikaczewski <guilhermecrikaczewski@gmail.com>
2025-08-19 15:44:01 -0300

    cria model de game image e relaciona com game

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,10 @@
 const sequelize = require('../config/database')
 const User = require('./userModel')(sequelize)
 const ProfilePic = require('./profilePicModel')(sequelize)
+const Game = require('./gameModel')(sequelize)
+const GameImage = require('./gameImageModel')(sequelize)
 
 User.belongsTo(ProfilePic, { foreignKey: 'idProfilePic' })
+Game.belongsTo(GameImage, { foreignKey: 'idGameImage' })
 
 module.exports = { User, sequelize}
