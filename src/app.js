90afe7a3fb41ec271b6dfd196a002e696a66878f
Your Name <guilhermecrikaczewski@gmail.com>
2025-08-19 20:47:02 -0300

    a

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const userRoutes = require('./routes/userRoutes')
 
 const app = express()
@@ -7,5 +8,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/users', userRoutes)
+app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
 module.exports=app
