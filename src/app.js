7d559d9e4d7d48bddd680c9e0691c24681becada
Guilherme C. Rikaczewski <guilhermecrikaczewski@gmail.com>
2025-08-20 18:55:50 -0300

    inferno de rota 404

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const cors = require('cors')
 const userRoutes = require('./routes/userRoutes')
+const path = require('path')
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(cors())
 app.use(express.json())
 app.use('/users', userRoutes)
+
+app.use((req, res)=>{
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
 
 module.exports=app
