22239d6d740e12a017cd31781ce9178ac67ab0ef
Guilherme Caique Rikaczewski <guilhermecrikaczewski@gmail.com>
2025-08-22 10:59:55 -0300

    adiciona relações entre Models

--- src/models/index.js
+++ src/models/index.js
@@ -4,8 +4,26 @@ const ProfilePic = require('./profilePicModel')(sequelize)
 const Game = require('./gameModel')(sequelize)
 const GameImage = require('./gameImageModel')(sequelize)
 const Role = require('./roleModel')(sequelize)
+const GameRule = require('./gameRuleModel')(sequelize)
 
+// belongsTo --> usado no lado onde a FK está --> referencia fk da tabela sendo usada
+// hasMany --> usado no lado que é referenciado pela FK --> referencia fk da tabela passada como parametro
+
+// Relações de User
 User.belongsTo(ProfilePic, { foreignKey: 'idProfilePic' })
+User.hasMany(GameRule, { foreignKey: 'idUser' })
+
+// Relações de Game
 Game.belongsTo(GameImage, { foreignKey: 'idGameImage' })
+Game.hasMany(GameRule)
+
+// Relações de GameImage
+GameImage.hasOne(Game, { foreignKey: 'idGameImage' })
+
+// Relações de GameRule
+GameRule.belongsTo(User, { foreignKey:'idUser' })
+GameRule.belongsTo(Role, { foreignKey: 'idRole' })
+GameRule.belongsTo(Game, { foreignKey: 'idGame' })
+
 
 module.exports = { User, sequelize}
