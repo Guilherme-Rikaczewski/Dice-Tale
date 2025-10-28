020d7ca6bb7cb9485bae75db5786f8214d9ed547
Guilherme C. Rikaczewski <guilhermecrikaczewski@gmail.com>
2025-10-28 14:48:54 -0300

    correção do conflito de codigo nas rotas de games

--- src/routes/gameRoutes.js
+++ src/routes/gameRoutes.js
@@ -5,10 +5,10 @@ const router = express.Router()
 
 router.post('/', authMiddleware, gameController.createGame) //testado
 router.post('/join/:code', authMiddleware, gameController.joinGame) //testado
-router.get('/:code', authMiddleware, gameController.getGameByCode) //testado
+router.get('/open/:code', authMiddleware, gameController.getGameByCode) //testado
 router.get('/', authMiddleware, gameController.getGameByName) //testado
-router.get('/all', authMiddleware, gameController.getAllGames)
-router.get('/rule', authMiddleware, gameController.getGameRule)
+router.get('/all', authMiddleware, gameController.getAllGames) // testado
+router.get('/rule', authMiddleware, gameController.getGameRule) //testado
 router.patch('/', authMiddleware, gameController.updateGame)
 router.delete('/', authMiddleware, gameController.deleteGame)
 
