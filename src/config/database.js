d15fc24fefb7ef5da2eb376b8594109536b502ab
Guilherme C. Rikaczewski <guilhermecrikaczewski@gmail.com>
2025-11-20 13:28:09 -0300

    conexões para o railway

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-        process.env.POSTGRES_DB,
-        process.env.POSTGRES_USER,
-        process.env.POSTGRES_PASSWORD,
-        {
-            host: process.env.DB_HOST,
-            dialect: 'postgres',
-            port: process.env.POSTGRES_PORT,
-            logging: false
-        }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false
+});
 
 module.exports = sequelize;
--- src/redis/client.js
+++ src/redis/client.js
@@ -4,7 +4,7 @@ const { notExist } = require('../utils/validators')
 require('dotenv').config()
 
 const client = createClient({
-    url: `redis://default:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+    url: process.env.REDIS_URL
 })
 client.connect()
 
