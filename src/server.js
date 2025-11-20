bce62bba64d56aa7af933ad6a0671e2d5995de0d
Guilherme C. Rikaczewski <guilhermecrikaczewski@gmail.com>
2025-11-19 22:49:03 -0300

    simple fix

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require('./app')
 require('dotenv').config()
 
 
-const port = 3069
+const port = process.env.SERVER_PORT
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
