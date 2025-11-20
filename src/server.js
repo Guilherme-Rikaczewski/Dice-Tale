a37ca348a4adbaf7771fc692173c14d130e303a4
Guilherme C. Rikaczewski <guilhermecrikaczewski@gmail.com>
2025-11-20 13:07:53 -0300

    escuta do servidor

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require('./app')
 require('dotenv').config()
 
 
-const port = process.env.SERVER_PORT
-app.listen(port, () => {
+const port = process.env.SERVER_PORT || 3000
+app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening on port ${port}`)
 })
